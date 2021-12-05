b443451fc987a92ac16c46b59a53535b98c3b30f
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-12-05 12:48:48 +0300

    добавление 26 урока

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 let url = 'https://jsonplaceholder.typicode.com/posts'
 const path = 'db.json'
 
-const sendData = (url, data) => {
+const sendData = (url,data) => {
 return fetch(url, {
   method: 'POST',
   body: JSON.stringify(data),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then(response => console.log(response))
-  .catch(error => console.log(error));
-
+  headers:{
+    'Content-Type': 'application/json; charset=UTF-8',
+  }
+}).then(response => response.json())
 }
 
-const getData = (path) => {
-  return fetch(path)
+const getData = (pathData) => {
+  return fetch(pathData)
     .then(response => response.json())
 } 
+
+getData(path).then(data => {
+  sendData(url, data)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+
+})
+
+  .catch(error => console.log(error))
 
 
 
